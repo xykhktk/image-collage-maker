f753c7640202892ce33a33c1e981ca6b392135f5
xuyukun <215920720@qq.com>
2024-09-02 00:38:33 +0800

    optimize download

--- src/components/Button/downloadButton.tsx
+++ src/components/Button/downloadButton.tsx
@@ -1,35 +1,34 @@
 "use client"
-import React, { useState } from 'react';
+import React from 'react';
 import { useCanvasData, useCanvasImageData } from "@/src/hooks/useReduxData"
 import {Button} from "@nextui-org/react";
-// import DownloadIcon from "@/src/components/DownloadIcon";
 import { DownloadIcon } from "@/src/components/icons";
 
 import { useRef } from "react"
 
 export default function DownloadButton() {
 
-    const linkRef = useRef<HTMLAnchorElement | null>(null)
     const { canvas } = useCanvasData()
     const { uploadCount, maxImageUploads } = useCanvasImageData()
 
     const downloadImage = () => {
-        if (canvas && linkRef.current) {
+        if (canvas) {
             canvas.discardActiveObject()
-            linkRef.current.href = canvas.toDataURL()
-            linkRef.current.download = `collage-${new Date().getTime()}.png`
-            linkRef.current.click()
+            const link = document.createElement("a"); 
+            link.href = canvas.toDataURL()
+            link.download = `collage-${new Date().getTime()}.png`
+            document.body.appendChild(link); 
+            link.click()
+            document.body.removeChild(link); 
         } else {
         }
     }
 
     return (
         <>
-            <a ref={linkRef} id="download" className="hidden"></a>
             <Button className="flex justify-center items-center transition-colors"
                     onClick={downloadImage}
                     radius="sm"
-                    // variant={uploadCount !== maxImageUploads ? "faded" : "shadow"}
                     color={uploadCount !== maxImageUploads ? "default" : "primary"}
                     disabled={uploadCount !== maxImageUploads}
             >
